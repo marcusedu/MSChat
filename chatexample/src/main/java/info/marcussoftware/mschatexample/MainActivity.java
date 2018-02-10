fca3df4ab386a22a261ff13b5bd205bb8411dd46
Marcus Eduardo <marcusedu7@gmail.com>
2018-02-10 12:08:07 -0200

    update chat example

--- chatexample/src/main/java/info/marcussoftware/mschatexample/MainActivity.java
+++ chatexample/src/main/java/info/marcussoftware/mschatexample/MainActivity.java
@@ -3,7 +3,6 @@ package info.marcussoftware.mschatexample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import java.util.Calendar;
 
@@ -12,6 +11,7 @@ import info.marcussoftware.mschat.view.contract.MSChatView;
 
 public class MainActivity extends AppCompatActivity implements MSChatPresenter {
     private MSChatView msChatView;
+    private String myUserId = "12";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements MSChatPresenter {
         setContentView(R.layout.activity_main);
         msChatView = findViewById(R.id.mainMSChat);
         msChatView.setPresenter(this);
+        msChatView.setSenderUserID(myUserId);
     }
 
     @Override
@@ -33,13 +34,15 @@ public class MainActivity extends AppCompatActivity implements MSChatPresenter {
 
     @Override
     public void userTyping(boolean typing, @Nullable String text) {
-//        Log.d(getClass().getSimpleName(), "userTyping: typing?:" + typing + "\nText:" + text);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setSubtitle(typing ? "User typing.." : null);
+        }
     }
 
     @Override
     public void userSendMessage(Calendar instance, String s) {
         MinhaMensagem message = new MinhaMensagem();
-        msChatView.showMessage(message.setDateTime(instance).setMessage(s).setUserName("Marcus").setUserId("1"));
-
+        msChatView.showMessage(message.setDateTime(instance).setMessage(s).setUserName("Marcus").setUserId(myUserId));
+        msChatView.showMessage(message.setDateTime(instance).setMessage(s).setUserName("Cliente").setUserId("13"));
     }
 }
