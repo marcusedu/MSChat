b70e8916ccda8dd4c54026382de431cd2b655bc3
Marcus Eduardo <marcusedu7@gmail.com>
2018-02-22 15:11:49 -0300

    change interface to abstract class

--- app/src/main/java/info/marcussoftware/mschat/interfaces/Message.java
+++ app/src/main/java/info/marcussoftware/mschat/interfaces/Message.java
@@ -6,29 +6,97 @@ import java.util.Calendar;
  * Created by Marcus Eduardo - marcusedu7@gmail.com on 21/01/2018.
  */
 
-public interface Message {
+public abstract class Message {
+    protected Calendar dateTime;
+    protected String userName, userId, message, messageId;
+
+    /**
+     * Setter dateTime
+     *
+     * @param dateTime
+     * @return
+     */
+    public Message setDateTime(Calendar dateTime) {
+        this.dateTime = dateTime;
+        return this;
+    }
+
+    /**
+     * Setter userName
+     *
+     * @param userName
+     * @return
+     */
+    public Message setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    /**
+     * Setter userId
+     *
+     * @param userId
+     * @return
+     */
+    public Message setUserId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    /**
+     * Setter message
+     *
+     * @param message
+     * @return
+     */
+    public Message setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    /**
+     * Setter messageId
+     *
+     * @param messageId
+     * @return
+     */
+    public Message setMessageId(String messageId) {
+        this.messageId = messageId;
+        return this;
+    }
+
     /**
      * @return A calendar with datetime that message has sended
      */
-    Calendar getDateTime();
+    public Calendar getDateTime() {
+        return dateTime;
+    }
 
     /**
      * @return Name of user that sended the message
      */
-    String getUserName();
+    public String getUserName() {
+        return userName;
+    }
 
     /**
      * @return Id of user that sended the message
      */
-    String getUserId();
+    public String getUserId() {
+        return userId;
+    }
 
     /**
      * @return Body of message
      */
-    String getMessage();
+    public String getMessage() {
+        return message;
+    }
 
     /**
      * @return Id of message
      */
-    String getMessageId();
+    public String getMessageId() {
+        return messageId;
+    }
 }
