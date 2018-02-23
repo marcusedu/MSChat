5eee0ed317f7c0ab8b43793076877c0d235c1b44
Marcus Eduardo <marcusedu7@gmail.com>
2018-02-23 03:49:41 -0300

    update sample to simulate message status

--- chatexample/src/main/java/info/marcussoftware/mschatexample/MainActivity.java
+++ chatexample/src/main/java/info/marcussoftware/mschatexample/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import java.util.Calendar;
 
+import info.marcussoftware.mschat.interfaces.Message;
 import info.marcussoftware.mschat.view.contract.MSChatPresenter;
 import info.marcussoftware.mschat.view.contract.MSChatView;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements MSChatPresenter {
     private MSChatView msChatView;
     private String myUserId = "12";
     private Handler mHandler = new Handler();
+    private int msgId = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +24,6 @@ public class MainActivity extends AppCompatActivity implements MSChatPresenter {
         msChatView = findViewById(R.id.mainMSChat);
         msChatView.setPresenter(this);
         msChatView.setSenderUserID(myUserId);
-    }
-
-    @Override
-    public void start() {
-
-    }
-
-    @Override
-    public void stop() {
-
     }
 
     @Override
@@ -44,17 +36,37 @@ public class MainActivity extends AppCompatActivity implements MSChatPresenter {
     @Override
     public void userSendMessage(Calendar dateMessage, String s) {
         MinhaMensagem message = new MinhaMensagem();
-        msChatView.showMessage(message.setDateTime(dateMessage).setMessage(s).setUserName("Marcus").setUserId(myUserId));
+        message.setMessageId("abc" + msgId++)
+                .setStatus(Message.Status.PENDING)
+                .setDateTime(dateMessage)
+                .setMessage(s)
+                .setUserName("Marcus")
+                .setUserId(myUserId);
+        msChatView.showMessage(message);
+
+        mHandler.postDelayed(() -> {
+            message.setStatus(Message.Status.SENDED);
+            msChatView.showMessage(message);
+        }, 500);
+        mHandler.postDelayed(() -> {
+            message.setStatus(Message.Status.RECEIVED);
+            msChatView.showMessage(message);
+        }, 1500);
+        mHandler.postDelayed(() -> {
+            message.setStatus(Message.Status.VISUALISED);
+            msChatView.showMessage(message);
+        }, 2500);
+
         mHandler.postDelayed(() -> {
             if (getSupportActionBar() != null) {
                 getSupportActionBar().setSubtitle("User typing..");
             }
             mHandler.postDelayed(() -> {
-                msChatView.showMessage(message.setDateTime(dateMessage).setMessage(s).setUserName("Cliente").setUserId("13"));
+                msChatView.showMessage(message.setMessageId("acc" + msgId++).setDateTime(dateMessage).setMessage(s).setUserName("Cliente").setUserId("13"));
                 if (getSupportActionBar() != null) {
                     getSupportActionBar().setSubtitle(null);
                 }
-            }, s.length() * 150);
-        }, ((int)( Math.random() * 5)) * 1000);
+            }, s.length() * 100);
+        }, (((int) (Math.random() * 5)) * 1000) + 2500);
     }
 }
