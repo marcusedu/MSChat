eb72d2ce6b547229eef1a0557865f88e18f71f60
Marcus Eduardo <marcusedu7@gmail.com>
2018-02-23 03:45:39 -0300

    status message layout implemented

--- app/src/main/java/info/marcussoftware/mschat/view/adapter/holders/SenderHolder.java
+++ app/src/main/java/info/marcussoftware/mschat/view/adapter/holders/SenderHolder.java
@@ -1,7 +1,10 @@
 package info.marcussoftware.mschat.view.adapter.holders;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import info.marcussoftware.mschat.R;
@@ -18,6 +21,7 @@ public class SenderHolder extends ChatHolder {
     private TextView mName;
     private TextView mMsg;
     private TextView mTime;
+    private ImageView mStatusIV;
 
     public SenderHolder(View itemView) {
         super(itemView);
@@ -25,6 +29,7 @@ public class SenderHolder extends ChatHolder {
         mName = itemView.findViewById(R.id.userNameTextView);
         mMsg = itemView.findViewById(R.id.msgTextView);
         mTime = itemView.findViewById(R.id.timeTextView);
+        mStatusIV = itemView.findViewById(R.id.messageStatus);
     }
 
     @Override
@@ -33,9 +38,32 @@ public class SenderHolder extends ChatHolder {
             mMsg.setText(chatWrapper.getMessage().getMessage());
             mTime.setText(DateUtil.formatTime(chatWrapper.getMessage().getDateTime()));
             mName.setText(chatWrapper.getMessage().getUserName());
+            switch (chatWrapper.getMessage().getStatus()) {
+                case PENDING:
+                    setIcon(R.drawable.ic_clock, R.color.gray80);
+                    break;
+                case ERROR:
+                    setIcon(R.drawable.ic_error, R.color.error);
+                    break;
+                case SENDED:
+                    setIcon(R.drawable.ic_check, R.color.gray80);
+                    break;
+                case RECEIVED:
+                    setIcon(R.drawable.ic_check_all, R.color.sender_color);
+                    break;
+                case VISUALISED:
+                    setIcon(R.drawable.ic_check_all, R.color.date_color);
+                    break;
+            }
         } else {
             Log.d(getClass().getSimpleName(), "bindData: Invalide Wrapper type");
         }
+    }
+
+    private void setIcon( int icRes, int color) {
+        Drawable icon = mStatusIV.getContext().getResources().getDrawable(icRes);
+        icon.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        mStatusIV.setBackground(icon);
     }
 
     @Override
